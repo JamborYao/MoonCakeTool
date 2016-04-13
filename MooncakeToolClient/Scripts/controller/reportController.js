18267e17caff28343a32ec645bdc46ffc9e57ef9
Edward-Zhou <v-tazho@hotmail.com>
2016-04-13 16:35:35 +0800

    table chart, line chart, pie chart demo

--- MooncakeToolClient/Scripts/controller/reportController.js
+++ MooncakeToolClient/Scripts/controller/reportController.js
@@ -1,7 +1,9 @@
-﻿controllers.controller('reportController', ['$scope', '$routeParams', 'columnChart', function ($scope, $routeParams, columnChart) {
-    google.charts.load("current", { packages: ['corechart'] });
-    google.charts.setOnLoadCallback(drawChart);
-    function drawChart() {
+﻿controllers.controller('reportController', ['$scope', '$routeParams', 'columnChart', 'tableChart', 'lineChart', 'pieChart', function ($scope, $routeParams, columnChart, tableChart, lineChart, pieChart) {
+    //load chart
+    google.charts.load("current", { packages: ['corechart', 'table', 'line', 'corechart'] });
+    //column chart    
+    google.charts.setOnLoadCallback(drawColumnChart);
+    function drawColumnChart() {
         var data = google.visualization.arrayToDataTable(columnChart.list);
 
         var view = new google.visualization.DataView(data);
@@ -21,7 +23,61 @@
             bar: { groupWidth: "95%" },
             legend: { position: "none" },
         };
-        var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+        var chart = new google.visualization.ColumnChart(document.getElementById("column_chart"));
         chart.draw(view, options);
+    }
+
+    //table chart   
+    google.charts.setOnLoadCallback(drawTableChart);
+
+    function drawTableChart() {
+        var data = new google.visualization.DataTable();
+        var data =google.visualization.arrayToDataTable(tableChart.data);
+        //var data = new google.visualization.DataTable();
+        //data.addColumn('string', 'Name');
+        //data.addColumn('number', 'Salary');
+        //data.addColumn('boolean', 'Full Time Employee');
+        //data.addRows([
+        //  ['Mike', { v: 10000, f: '$10,000' }, true],
+        //  ['Jim', { v: 8000, f: '$8,000' }, false],
+        //  ['Alice', { v: 12500, f: '$12,500' }, true],
+        //  ['Bob', { v: 7000, f: '$7,000' }, true]
+        //]);
+        var table = new google.visualization.Table(document.getElementById('table_chart'));
+
+        table.draw(data, { showRowNumber: true, width: '100%', height: '100%' });
+    }
+
+    //line chart
+    google.charts.setOnLoadCallback(drawLineChart);
+    function drawLineChart() {
+        var data = google.visualization.arrayToDataTable(lineChart.data);
+        var options = {
+            hAxis: {
+                title: 'Time'
+            },
+            vAxis: {
+                title: 'Popularity'
+            }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('line_Chart'));
+
+        chart.draw(data, options);
+    }
+
+    //pie chart
+    google.charts.setOnLoadCallback(drawPieChart);
+    function drawPieChart() {
+        var data = google.visualization.arrayToDataTable(pieChart.data);
+
+        var options = {
+            title: 'My Daily Activities',
+            width: '100%',
+            height:'100%',
+            is3D: true,
+        };
+        var chart = new google.visualization.PieChart(document.getElementById('pie_Chart'));
+        chart.draw(data, options);
     }
 }]);
