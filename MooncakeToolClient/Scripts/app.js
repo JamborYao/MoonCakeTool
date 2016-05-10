e4fa34e96b4b28ff24890711293c4276e935d4a7
Edward-Zhou <v-tazho@hotmail.com>
2016-05-10 16:19:31 +0800

    test

--- MooncakeToolClient/Scripts/app.js
+++ MooncakeToolClient/Scripts/app.js
@@ -1,27 +1,63 @@
-﻿var baseUrl = "http://localhost:8929/";
+﻿/// <reference path="angular-ui-router.js" />
 
-var controllers = angular.module('controllers', []);
+var baseUrl = "http://localhost:8929/";
+
+var controllers = angular.module('controllers', ['ui.router']);
 
 
-var app = angular.module('_app_', ['ngRoute', 'controllers','git']);
+var app = angular.module('_app_', ['ui.router']);
 
-app.config(['$routeProvider',
-  function ($routeProvider) {
-      //google.charts.load("current", { packages: ['corechart', 'table', 'line', 'corechart'] });
-      $routeProvider.
-        when('/report/', {
-            templateUrl: 'partials/report.html',
-            controller: 'reportController'
-        }).
-        when('/github/', {
-            templateUrl: 'partials/github.html',
-            controller: 'githubController'
-        }).
-        otherwise({
-            redirectTo:'/github/'
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('index', {
+            templateUrl:'partials/index.html'
         })
+        .state('index.test', {
+            templateUrl: 'partials/test.html'
+        })
+});
+app.controller('MainCtrl', function ($state) {
+    $state.transitionTo('index.test');
+});
+//app.config(function ($stateProvider, $urlRouterProvider) {
+//    $stateProvider
+//        .state('index', {
+//            templateUrl: 'partials/reportGoogle.html'            
+//        })
+//    .state('github', {
+//        views: {
+//            'navtop': {
+//                url: '/',
+//                templateUrl: 'partials/navtop.html'
+//            },
+//            'navleft': {
+//                url: '/',
+//                templateUrl: 'partials/navleft.html',
+//                controller: 'navleftController'
+//            }
+//        }
+//    })
+//}).run(['$state', function ($state) {
+//    $state.transitionTo('index');
+//}]);
 
-  }]);
+//app.config(['$routeProvider',
+//  function ($routeProvider) {
+//      //google.charts.load("current", { packages: ['corechart', 'table', 'line', 'corechart'] });
+//      $routeProvider.
+//        when('/report/', {
+//            templateUrl: 'partials/report.html',
+//            controller: 'reportController'
+//        }).
+//        when('/github/', {
+//            templateUrl: 'partials/github.html',
+//            controller: 'githubController'
+//        }).
+//        otherwise({
+//            redirectTo:'/github/'
+//        })
+
+//  }]);
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
---
+++ MooncakeToolClient/Scripts/controller/navleftController.js
@@ -0,0 +1,7 @@
+﻿controllers.controller('navleftController', ['$scope', '$state',
+    function ($scope,$state) {
+        $scope.navigateTo = function (module) {
+            console.log('index.github');
+            $state.transitionTo('github');
+        }
+    }]);
